8f8f2fba88859e57e8bbd0bfcada6b4a778fa6f4
e-picco <enrico.picco@hotmail.it>
2026-05-22 15:43:14 +0200

    Delete Python code directory

--- Python code/functions/hameg power supply/hameg_set_volt.py
+++
@@ -1,9 +0,0 @@
-def hameg_set_volt(obj, channel, voltage):
-    """
-    Sets specific power output values onto physical HAMEG instrumentation channels.
-    """
-    command_sel = f"INST:NSEL {channel}\n"
-    command_volt = f"VOLT {voltage}\n"
-    
-    obj.write(command_sel.encode('utf-8'))
-    obj.write(command_volt.encode('utf-8'))
